78abb00b9588b4510c081533976a06f6f280e60d
otinanism <otinanism@gmail.com>
2017-09-15 17:22:25 +0300

    revert back to login form as the angular interceptor finally works

--- src/main/java/gr/alx/startup/WebSecurityConfig.java
+++ src/main/java/gr/alx/startup/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sameOrigin()
 
                 .and()
-                .httpBasic()
+                .formLogin()
         ;
     }
 
