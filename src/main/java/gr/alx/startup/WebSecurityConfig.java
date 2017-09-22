880f7bd4b9eaf92328f1e3e47bd0d27886656d43
otinanism <otinanism@gmail.com>
2017-09-22 11:07:33 +0300

    fix xsrf token working (GET requests do not need XSRF!)

--- src/main/java/gr/alx/startup/WebSecurityConfig.java
+++ src/main/java/gr/alx/startup/WebSecurityConfig.java
@@ -10,11 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import java.util.regex.Pattern;
 
 
 @EnableWebSecurity
@@ -45,6 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf()
                 .requireCsrfProtectionMatcher(csrfRequestMatcher)
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 
                 .and()
                 .authorizeRequests()
@@ -69,16 +72,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     RequestMatcher csrfRequestMatcher = new RequestMatcher() {
 
+        // Always allow the HTTP GET method
+        private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+
         // Disable CSFR protection on the following urls:
         private AntPathRequestMatcher[] requestMatchers = {
                 new AntPathRequestMatcher("/login"),
                 new AntPathRequestMatcher("/logout"),
                 new AntPathRequestMatcher("/h2-console/**"),
-                new AntPathRequestMatcher("/api/**")
+//                new AntPathRequestMatcher("/api/**")
         };
 
         @Override
         public boolean matches(HttpServletRequest request) {
+            // Skip allowed methods
+            if (allowedMethods.matcher(request.getMethod()).matches()) {
+                return false;
+            }
+
             // If the request match one url the CSFR protection will be disabled
             for (AntPathRequestMatcher rm : requestMatchers) {
                 if (rm.matches(request)) {
