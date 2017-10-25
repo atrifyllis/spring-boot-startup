0afc696d1a53553e2f10aef4b8ef18cff90a3fea
otinanism <otinanism@gmail.com>
2017-10-25 16:28:26 +0300

    add enpoint to retrieve logged in user info

---
+++ src/main/java/gr/alx/startup/gr/alx/startup/user/AuthenticatedUserResource.java
@@ -0,0 +1,25 @@
+package gr.alx.startup.gr.alx.startup.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.stream.Collectors.toSet;
+
+// TODO: Tried to use the @org.springframework.data.rest.webmvc.BasePathAwareController but ti wouldn't work
+@RestController
+public class AuthenticatedUserResource {
+
+    @GetMapping(value = "/api/auth/me")
+    public User getAuthenticatedUser(Authentication authentication) {
+        // Retrieve also email somehow?
+        return User.builder()
+                .username(authentication.getName())
+                .roles(authentication.getAuthorities().stream()
+                        .map(o -> o.getAuthority())
+                        .map(s -> Role.valueOf(s))
+                        .collect(toSet())
+                )
+                .build();
+    }
+}
