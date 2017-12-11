9025e619b09ba29f8bc00449fe723bf410bd95b7
otinanism <otinanism@gmail.com>
2017-12-11 11:24:22 +0200

    add self link in response of AuthenticatedUserResource

--- src/main/java/gr/alx/startup/gr/alx/startup/user/AuthenticatedUserResource.java
+++ src/main/java/gr/alx/startup/gr/alx/startup/user/AuthenticatedUserResource.java
@@ -1,9 +1,15 @@
 package gr.alx.startup.gr.alx.startup.user;
 
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 import static java.util.stream.Collectors.toSet;
 
@@ -12,9 +18,9 @@ import static java.util.stream.Collectors.toSet;
 public class AuthenticatedUserResource {
 
     @GetMapping(value = "/api/auth/me")
-    public User getAuthenticatedUser(Authentication authentication) {
+    public ResponseEntity<Resource<User>> getAuthenticatedUser(Authentication authentication) {
         // Retrieve also email somehow?
-        return User.builder()
+        User user = User.builder()
                 .username(authentication.getName())
                 .roles(authentication.getAuthorities().stream()
                         .map(GrantedAuthority::getAuthority)
@@ -22,5 +28,13 @@ public class AuthenticatedUserResource {
                         .collect(toSet())
                 )
                 .build();
+        return ResponseEntity.ok(getUserResource(user));
+    }
+
+    private Resource<User> getUserResource(User user) {
+        Resource<User> userResource = new Resource<>(user);
+        URI self = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+        userResource.add(new Link(self.toString(), "self"));
+        return userResource;
     }
 }
