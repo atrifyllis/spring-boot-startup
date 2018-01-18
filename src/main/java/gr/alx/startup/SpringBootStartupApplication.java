e6f20cc0d1bdcbf025eb36cda197d327c120ad40
otinanism <otinanism@gmail.com>
2018-01-19 01:15:05 +0200

    add mysql db config for prod/cloud profiles

--- src/main/java/gr/alx/startup/SpringBootStartupApplication.java
+++ src/main/java/gr/alx/startup/SpringBootStartupApplication.java
@@ -1,8 +1,12 @@
 package gr.alx.startup;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,22 @@ public class SpringBootStartupApplication {
         SpringApplication.run(SpringBootStartupApplication.class, args);
     }
 
+    /**
+     * Repair database before migration. This seems that it should be default...
+     *
+     * @return
+     */
+    @Bean
+    @Profile({"prod", "cloud"})
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
+            @Override
+            public void migrate(Flyway flyway) {
+                flyway.repair();
+                flyway.migrate();
+            }
+        };
+
+        return strategy;
+    }
 }
